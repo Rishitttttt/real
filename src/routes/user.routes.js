0462baea45652d1c4ac321309240bad0d4a1b3f2
rishit <rishitkhirbat@gmail.com>
2026-01-27 07:36:33 +0530

    updating

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,14 @@ import {
   registerUser,
   loginUser,
   logoutUser,
-  refreshAccessToken
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 } from "../controllers/user.controller.js";
 
 import  { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,13 @@ router.post("/login", loginUser);
 
 router.post("/logout", verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").put(verifyJWT, updateAccountDetails);
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+
 
 export default router;
